cadf8bb709c903fe521faaa69cddf0d39e519214
aichetou1408 <aichetouahmedgaye@gmail.com>
2024-10-08 09:36:51 +0100

    second commit

--- src/app.js
+++ src/app.js
@@ -6,7 +6,3 @@ const app = express()
 app.listen(3000, (req, res) => {
     console.log("Connected with express");
 })
-
-app.get("/api/private-data", (req, res) => {
-    res.json({ message: "Hello world!"})
-})
